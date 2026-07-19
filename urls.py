589ebf9f6483870a961ce9bd16f77b3031af844a
nplatte <77999574+nplatte@users.noreply.github.com>
2026-07-19 15:47:21 -0400

    removed landing url

--- urls.py
+++ urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from login import views as login_views
 
 urlpatterns = [
-    path('', login_views.LoginView.as_view(), name='login'),
-    path('landing', login_views.landing, name='landing'),
+    path('', login_views.LoginView.as_view(), name='login')
 ]
