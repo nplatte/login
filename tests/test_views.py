f809c26cede301ac1e38fce59c196066fd1fea32
Nathan Platte <nplatte0@gmail.com>
2026-07-23 08:11:19 -0400

    corrected login tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,21 +6,23 @@ class TestLogin(TestCase):
 
     fixtures = ['users']
 
+    def setUp(self):
+        self.data = {'username': "testuser", 'password': 'thisiscorrect'}
+        return super().setUp()
+        
     def test_GET(self):
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_login_success(self):
-        data = {'username': "nplatte", 'password': 'Sparta12456@'}
-        response = self.client.post(reverse('login'), data)
+        response = self.client.post(reverse('login'), self.data)
         self.assertEqual(response.status_code, 302)
 
     def test_good_login_redirect_to_landing(self):
-        data = {'username': "nplatte", 'password': 'Sparta12456@'}
-        response = self.client.post(reverse('login'), data)
+        response = self.client.post(reverse('login'), self.data)
         self.assertRedirects(response, reverse('landing'))
 
     def test_user_login_fail(self):
-        data = {'username': "TestUser1", 'password': 'thisiswrong'}
-        response = self.client.post(reverse('login'), data)
+        self.data['password'] = 'wrong'
+        response = self.client.post(reverse('login'), self.data)
         self.assertEqual(response.status_code, 200)
